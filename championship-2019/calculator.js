908c6efa9132164ded08c59ea3759468c9cbfa0e
Paul Fryers <paul.fryers@gmail.com>
2022-09-28 14:57:14 +0100

    Add old competitions

---
+++ championship-2019/calculator.js
@@ -0,0 +1,17 @@
+function updateOutput() {
+  var form = document.getElementById("calculator");
+  var points = form.elements["points"];
+  var yourtime = parseInt(form.elements["yourtimeS"].value) + parseInt(form.elements["yourtimeM"].value) * 60 + parseInt(form.elements["yourtimeH"].value) * 3600;
+  var wintime = parseInt(form.elements["wintimeS"].value) + parseInt(form.elements["wintimeM"].value) * 60 + parseInt(form.elements["wintimeH"].value) * 3600;
+  console.log(yourtime);
+  console.log(wintime);
+  if (yourtime == 0 || wintime == 0){
+    points.value = "Error: Times must not be zero";
+    return;
+  }
+  if (wintime > yourtime) {
+    points.value = "Error: Your time must not be shorter than the winning time";
+    return;
+  }
+  points.value = String((100 - 50 * Math.log(wintime / yourtime) / Math.log(0.5)).toFixed(5)).replace("-", "−");
+}
---
+++ fell-league-2021/calculator.js
@@ -0,0 +1,23 @@
+function updateOutput() {
+  const form = document.getElementById("calculator");
+  const points = form.elements["points"];
+  const yourtime =
+    parseInt(form.elements["yourtimeS"].value) +
+    parseInt(form.elements["yourtimeM"].value) * 60 +
+    parseInt(form.elements["yourtimeH"].value) * 3600;
+  const wintime =
+    parseInt(form.elements["wintimeS"].value) +
+    parseInt(form.elements["wintimeM"].value) * 60 +
+    parseInt(form.elements["wintimeH"].value) * 3600;
+  if (yourtime === 0 || wintime === 0) {
+    points.value = "Error: Times must not be zero";
+    return;
+  }
+  if (wintime > yourtime) {
+    points.value = "Error: Your time must not be shorter than the winning time";
+    return;
+  }
+  points.value = String(
+    (100 - (50 * Math.log(wintime / yourtime)) / Math.log(0.5)).toFixed(5)
+  ).replace("-", "−");
+}
